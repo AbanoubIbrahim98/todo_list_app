8f3ee84aa13f6911470b6f30e38a2dad4ba95028
Your Name <abanoubhabak98@gmail.com>
2022-05-13 00:19:32 +0200

    first commit

---
+++ lib/constants/theme.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+
+class Theme {
+  Color white = Colors.white;
+  Color purple = Colors.purple;
+  Color pink = Colors.pink;
+  Color blue = Colors.lightBlueAccent;
+  Color yellow = Colors.yellow;
+  Color lime = Colors.lime;
+  Color green = Colors.green;
+  Color grey = Colors.grey;
+  Color teal = Colors.teal;
+  Theme(
+      {this.white,
+      this.blue,
+      this.green,
+      this.grey,
+      this.lime,
+      this.pink,
+      this.purple,
+      this.teal,
+      this.yellow});
+}
